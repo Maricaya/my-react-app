fdfaf40472240c1195899e176b37cc9164e70931
Maricaya <915270549@qq.com>
2021-11-13 15:36:31 +0800

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -14,4 +14,18 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+console.log({navigator})
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        // console.log({serviceWorker})
+
+        navigator.serviceWorker.register('/assets/service-worker.js').then(registration => {
+            console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
+
 reportWebVitals();
